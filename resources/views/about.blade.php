0e4c71c810540491f391cf4d7e368f217c5af3e4
Zahra Naziha <nazihazahra370@gmail.com>
2024-12-02 14:19:11 +0700

    ubah footer dan about us

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,8 @@
 @section('content')
     <div class="container text-center"> <!-- Tambahkan class text-center -->
         <h1>About Us</h1>
-        <p>Bless merupakan website kosmetik</p>
+        <p>Toko Bless Kosmetik adalah destinasi kecantikan yang hadir untuk memenuhi kebutuhan perempuan Indonesia akan produk kecantikan berkualitas tinggi. Kami menyediakan berbagai pilihan produk kosmetik, skincare, dan perawatan rambut dari berbagai merek lokal dan internasional. Misi kami adalah meningkatkan kecantikan dan kepercayaan diri perempuan Indonesia melalui rangkaian produk yang dirancang untuk mempercantik, merawat, dan memberikan kepuasan setiap harinya.</p>
+        <p>Dengan tagline “Glow up, starts here”, “Pamper yourself, you deserve it”, dan “From skincare to self-care”, kami berkomitmen untuk memberikan pengalaman berbelanja yang menyenangkan, dimulai dari kemasan produk yang menarik hingga formula berkualitas tinggi yang aman dan efektif. Kami juga berpedoman pada prinsip cruelty-free, tanpa melakukan pengujian pada hewan, dan selalu mengutamakan produk dengan bahan yang aman dan ramah di kulit.</p>
+        <p>Toko Bless Kosmetik selalu mengikuti tren kecantikan global dan mendengarkan kebutuhan pelanggan, sehingga kami dapat terus berinovasi dan menyediakan produk-produk terbaik yang sesuai dengan keinginan dan kebutuhan Anda. Kami ingin setiap perempuan merasa lebih percaya diri dan cantik setiap hari dengan produk-produk dari Toko Bless.</p>
     </div>
 @endsection
